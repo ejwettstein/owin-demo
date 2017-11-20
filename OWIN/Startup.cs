b96a8541c1453111e7de523e272d0e17d1b52284
Eric Wettstein <ewettstein@ravinia.org>
2017-11-20 16:23:19 -0600

    Testing OWIN

---
+++ OWIN/Startup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Owin;
+
+namespace OWINTest
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseErrorPage(new Microsoft.Owin.Diagnostics.ErrorPageOptions()
+            {
+                ShowExceptionDetails = true
+            });
+            app.Run(context =>
+            {
+                if (context.Request.Path.Value == "/fail")
+                {
+                    throw new Exception("Random exception");
+                }
+
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
